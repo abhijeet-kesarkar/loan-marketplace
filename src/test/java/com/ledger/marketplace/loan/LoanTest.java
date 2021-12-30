b2b8d213ed7ac0d29805dae5360495dc506963b7
Abhijeet Kesarkar <abhijit.kesar@gmail.com>
2021-12-30 16:40:07 +1100

    Add test for different borrower

--- src/test/java/com/ledger/marketplace/loan/LoanTest.java
+++ src/test/java/com/ledger/marketplace/loan/LoanTest.java
@@ -6,10 +6,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoanTest {
     @Test
-    public void testBalance(){
+    public void testBalanceForBorrowerDale(){
         Loan loan = new Loan("IDIDI", "Dale", 10000, 5, 4);
         Balance balance = loan.balance(5);
         assertEquals(1000, balance.getAmountPaid());
         assertEquals(55, balance.getNoOfEMIsRemaining());
+    }
+
+    @Test
+    public void testBalanceForBorrowerHarry(){
+        Loan loan = new Loan("MBI", "Harry", 2000, 2, 2);
+        Balance balance = loan.balance(12);
+        assertEquals(1044, balance.getAmountPaid());
+        assertEquals(12, balance.getNoOfEMIsRemaining());
     }
 }
