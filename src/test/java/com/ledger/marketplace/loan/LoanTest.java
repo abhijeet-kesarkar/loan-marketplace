c1a505754b62580b26e279b9d97e53272946ffb8
Abhijeet Kesarkar <abhijit.kesar@gmail.com>
2021-12-31 16:09:47 +1100

    Add test for payment

--- src/test/java/com/ledger/marketplace/loan/LoanTest.java
+++ src/test/java/com/ledger/marketplace/loan/LoanTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoanTest {
     @Test
-    public void testBalanceForDaleAfter5EMIs(){
+    public void testBalanceForDaleAfter5EMIs() {
         Loan loan = new Loan("IDIDI", "Dale", 10000, 5, 4);
         Balance balance = loan.balance(5);
         assertEquals(1000, balance.getAmountPaid());
@@ -14,7 +14,7 @@ public class LoanTest {
     }
 
     @Test
-    public void testBalanceForDaleAfter40EMIs(){
+    public void testBalanceForDaleAfter40EMIs() {
         Loan loan = new Loan("IDIDI", "Dale", 10000, 5, 4);
         Balance balance = loan.balance(40);
         assertEquals(8000, balance.getAmountPaid());
@@ -22,7 +22,7 @@ public class LoanTest {
     }
 
     @Test
-    public void testBalanceForHarryAfter12EMIs(){
+    public void testBalanceForHarryAfter12EMIs() {
         Loan loan = new Loan("MBI", "Harry", 2000, 2, 2);
         Balance balance = loan.balance(12);
         assertEquals(1044, balance.getAmountPaid());
@@ -30,7 +30,7 @@ public class LoanTest {
     }
 
     @Test
-    public void testBalanceForHarryAfter0EMIs(){
+    public void testBalanceForHarryAfter0EMIs() {
         Loan loan = new Loan("MBI", "Harry", 2000, 2, 2);
         Balance balance = loan.balance(0);
         assertEquals(0, balance.getAmountPaid());
@@ -38,10 +38,23 @@ public class LoanTest {
     }
 
     @Test
-    public void testBalanceForHarryAfter24EMIs(){
+    public void testBalanceForHarryAfter24EMIs() {
         Loan loan = new Loan("MBI", "Harry", 2000, 2, 2);
         Balance balance = loan.balance(24);
         assertEquals(2080, balance.getAmountPaid());
         assertEquals(0, balance.getNoOfEMIsRemaining());
+    }
+
+    @Test
+    public void testPaymentForDaleAfter5thEMI() {
+        Loan loan = new Loan("IDIDI", "Dale", 5000, 1, 6);
+        loan.payment(1000, 5);
+        Balance balanceAfter3EMIs = loan.balance(3);
+        assertEquals(1326, balanceAfter3EMIs.getAmountPaid());
+        assertEquals(9, balanceAfter3EMIs.getNoOfEMIsRemaining());
+
+        Balance balanceAfter6EMIs = loan.balance(6);
+        assertEquals(3652, balanceAfter6EMIs.getAmountPaid());
+        assertEquals(4, balanceAfter6EMIs.getNoOfEMIsRemaining());
     }
 }
