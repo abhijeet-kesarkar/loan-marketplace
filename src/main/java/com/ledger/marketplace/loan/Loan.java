79a603633a0cc6b28dc1350be9228fa2066b3cc2
Abhijeet Kesarkar <abhijit.kesar@gmail.com>
2021-12-31 11:03:16 +1100

    Update balance logic for tail emi

--- src/main/java/com/ledger/marketplace/loan/Loan.java
+++ src/main/java/com/ledger/marketplace/loan/Loan.java
@@ -5,9 +5,9 @@ public class Loan {
     private final String borrowerName;
     private final int principal;
     private final int noOfYears;
-    private final int rateOfInterest;
+    private final double rateOfInterest;
 
-    public Loan(String bankName, String borrowerName, int principal, int noOfYears, int rateOfInterest) {
+    public Loan(String bankName, String borrowerName, int principal, int noOfYears, double rateOfInterest) {
 
         this.bankName = bankName;
         this.borrowerName = borrowerName;
@@ -17,11 +17,18 @@ public class Loan {
     }
 
     public Balance balance(int emiNo) {
-        int interest = principal * noOfYears * rateOfInterest / 100;
+        double interest = principal * noOfYears * rateOfInterest / 100;
         double amount = principal + interest;
         int emi = (int) Math.ceil(amount / (noOfYears * 12));
-        int amountPaid = emiNo * emi;
-        int noOfEMIsRemaining = (int) Math.ceil((amount - amountPaid) / emi);
+
+        int amountPaid = 0;
+        double balanceAmount = amount;
+        for( int i=0; i<emiNo; i++)
+        {
+            amountPaid += balanceAmount < emi ? balanceAmount : emi;
+            balanceAmount = amount - amountPaid;
+        }
+        int noOfEMIsRemaining = (int) Math.ceil(balanceAmount / emi);
         return new Balance(amountPaid, noOfEMIsRemaining);
     }
 }
