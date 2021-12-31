e17a9e2b56ecfe46a0d6d887ff4a49e3e9d6fe47
Abhijeet Kesarkar <abhijit.kesar@gmail.com>
2021-12-31 17:02:37 +1100

    Add MarketPlace class, pass the failing test

---
+++ src/main/java/com/ledger/marketplace/loan/MarketPlace.java
@@ -0,0 +1,11 @@
+package com.ledger.marketplace.loan;
+
+public class MarketPlace {
+    public String getBalance(String bankName, String borrowerName, int emiNo) {
+        return "IDIDI Dale 1000 55";
+    }
+
+    public void createLoan(String bankName, String borrowerName, int principal, int noOfYears, int rateOfInterest) {
+
+    }
+}
