aff24f6a6e71a45a97760b4f3d8f158e1f045191
Abhijeet Kesarkar <abhijit.kesar@gmail.com>
2021-12-31 17:16:18 +1100

    Add another test for add loan in marketplace test

--- src/test/java/com/ledger/marketplace/loan/MarketPlaceTest.java
+++ src/test/java/com/ledger/marketplace/loan/MarketPlaceTest.java
@@ -7,12 +7,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MarketPlaceTest {
 
     @Test
-    public void testCreateLoan(){
+    public void testCreateLoanForDale(){
         MarketPlace marketPlace = new MarketPlace();
         marketPlace.createLoan("IDIDI", "Dale", 10000, 5, 4);
 
         String balance = marketPlace.getBalance("IDIDI", "Dale", 5);
 
         assertEquals("IDIDI Dale 1000 55", balance);
+    }
+
+    @Test
+    public void testCreateLoanForHarry(){
+        MarketPlace marketPlace = new MarketPlace();
+        marketPlace.createLoan("MBI", "Harry", 2000, 2, 2);
+
+        String balance = marketPlace.getBalance("MBI", "Harry", 12);
+
+        assertEquals("MBI Harry 1044 12", balance);
     }
 }
