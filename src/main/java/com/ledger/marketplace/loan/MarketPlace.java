59c1a76e8c803864d49a355a1a61e9f34898a651
Abhijeet Kesarkar <abhijit.kesar@gmail.com>
2021-12-31 17:17:30 +1100

    Implement create loan and get balance in MarketPlace

--- src/main/java/com/ledger/marketplace/loan/MarketPlace.java
+++ src/main/java/com/ledger/marketplace/loan/MarketPlace.java
@@ -1,11 +1,20 @@
 package com.ledger.marketplace.loan;
 
+import java.util.HashMap;
+
 public class MarketPlace {
+
+    private HashMap<String, Loan> loans = new HashMap<>();
+
     public String getBalance(String bankName, String borrowerName, int emiNo) {
-        return "IDIDI Dale 1000 55";
+        return loans.get(getKey(bankName, borrowerName)).balance(emiNo).toString();
     }
 
     public void createLoan(String bankName, String borrowerName, int principal, int noOfYears, int rateOfInterest) {
+        loans.put(getKey(bankName, borrowerName), new Loan(bankName, borrowerName, principal, noOfYears, rateOfInterest));
+    }
 
+    private String getKey(String bankName, String borrowerName) {
+        return (bankName + borrowerName).toUpperCase();
     }
 }
