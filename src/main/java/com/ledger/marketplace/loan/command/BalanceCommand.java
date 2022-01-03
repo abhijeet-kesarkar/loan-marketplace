4dc991af0d99dc48c6a91ecfa982b2d90b8b15e9
Abhijeet Kesarkar <abhijit.kesar@gmail.com>
2022-01-03 16:35:54 +1100

    Implement balance execute command

--- src/main/java/com/ledger/marketplace/loan/command/BalanceCommand.java
+++ src/main/java/com/ledger/marketplace/loan/command/BalanceCommand.java
@@ -7,17 +7,20 @@ import java.io.PrintStream;
 public class BalanceCommand extends Command {
 
 
+    private int emiNo;
+
     public BalanceCommand(MarketPlace marketPlace, String bankName, String borrowerName, String commandParams) {
         super(marketPlace, bankName, borrowerName, commandParams);
     }
 
     @Override
     protected void parseParams(String commandParams) {
-
+        String[] params = commandParams.split("\\s");
+        this.emiNo = Integer.parseInt(params[0]);
     }
 
     @Override
     public void execute(PrintStream... out) {
-        out[0].println("IDIDI Dale 1000 55");
+        out[0].println(marketPlace.getBalance(bankName, borrowerName, emiNo));
     }
 }
