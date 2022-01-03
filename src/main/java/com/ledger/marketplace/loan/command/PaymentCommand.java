286b57819d35094510b034c11304f8913f64410f
Abhijeet Kesarkar <abhijit.kesar@gmail.com>
2022-01-03 15:35:01 +1100

    Implement logic for execute payment command

--- src/main/java/com/ledger/marketplace/loan/command/PaymentCommand.java
+++ src/main/java/com/ledger/marketplace/loan/command/PaymentCommand.java
@@ -4,6 +4,8 @@ import com.ledger.marketplace.loan.MarketPlace;
 
 public class PaymentCommand extends Command {
 
+    private int lumpSumAmount;
+    private int emiNo;
 
     public PaymentCommand(MarketPlace marketPlace, String bankName, String borrowerName, String commandParams) {
         super(marketPlace, bankName, borrowerName, commandParams);
@@ -11,11 +13,13 @@ public class PaymentCommand extends Command {
 
     @Override
     protected void parseParams(String commandParams) {
-
+        String[] params = commandParams.split("\\s");
+        lumpSumAmount = Integer.parseInt(params[0]);
+        emiNo = Integer.parseInt(params[1]);
     }
 
     @Override
     public void execute() {
-        this.marketPlace.makePayment("UON", "Shelly", 7000, 12);
+        this.marketPlace.makePayment(bankName, borrowerName, lumpSumAmount, emiNo);
     }
 }
