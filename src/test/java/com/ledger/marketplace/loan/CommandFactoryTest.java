a834801a4067a434319da5eb116d78d14b2a191e
Abhijeet Kesarkar <abhijit.kesar@gmail.com>
2022-01-03 14:05:37 +1100

    Refactor CommandFactoryTest to use beforeEach

--- src/test/java/com/ledger/marketplace/loan/CommandFactoryTest.java
+++ src/test/java/com/ledger/marketplace/loan/CommandFactoryTest.java
@@ -1,45 +1,42 @@
 package com.ledger.marketplace.loan;
 
 import com.ledger.marketplace.loan.command.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CommandFactoryTest {
+    MarketPlace marketPlace;
+    CommandFactory commandFactory;
+
+    @BeforeEach
+    public void setup() {
+        marketPlace = new MarketPlace();
+        commandFactory = new CommandFactory(marketPlace);
+    }
 
     @Test
     public void testCreateLoanCommand() throws Exception {
-
-        MarketPlace marketPlace = new MarketPlace();
-        CommandFactory commandFactory = new CommandFactory(marketPlace);
         Command command = commandFactory.create("LOAN bank borrower 100 1 1");
         assertEquals(LoanCommand.class, command.getClass());
     }
 
     @Test
     public void testCreatePaymentCommand() throws Exception {
-
-        MarketPlace marketPlace = new MarketPlace();
-        CommandFactory commandFactory = new CommandFactory(marketPlace);
         Command command = commandFactory.create("PAYMENT bank borrower 100 1");
         assertEquals(PaymentCommand.class, command.getClass());
     }
 
     @Test
     public void testCreateBalanceCommand() throws Exception {
-
-        MarketPlace marketPlace = new MarketPlace();
-        CommandFactory commandFactory = new CommandFactory(marketPlace);
         Command command = commandFactory.create("BALANCE bank borrower 100 1");
         assertEquals(BalanceCommand.class, command.getClass());
     }
 
     @Test
     public void testInvalidCommand() {
-
-        MarketPlace marketPlace = new MarketPlace();
-        CommandFactory commandFactory = new CommandFactory(marketPlace);
         assertThrows(Exception.class, () -> commandFactory.create("some bank borrower 100 1"));
     }
 }
