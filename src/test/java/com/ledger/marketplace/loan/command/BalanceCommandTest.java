941129da7aa49f803bc60448482a174253fe43b1
Abhijeet Kesarkar <abhijit.kesar@gmail.com>
2022-01-03 16:22:53 +1100

    Add test to execute balance command

---
+++ src/test/java/com/ledger/marketplace/loan/command/BalanceCommandTest.java
@@ -0,0 +1,26 @@
+package com.ledger.marketplace.loan.command;
+
+import com.ledger.marketplace.loan.MarketPlace;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BalanceCommandTest {
+
+    @Test
+    void executeBalanceCommandForDale() {
+        MarketPlace marketPlace = new MarketPlace();
+        marketPlace.createLoan("IDIDI", "Dale", 10000, 5, 4);
+
+        Command command = new BalanceCommand(marketPlace, "IDIDI", "Dale", "5");
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        command.execute(new PrintStream(outputStream));
+
+        assertEquals("IDIDI Dale 1000 55\n", outputStream.toString());
+
+    }
+}
