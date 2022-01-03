3f5abf6c21a325776c1bc5f2616c3f7f43df36a9
Abhijeet Kesarkar <abhijit.kesar@gmail.com>
2022-01-03 15:32:57 +1100

    Add test for another borrower to execute payment command

--- src/test/java/com/ledger/marketplace/loan/command/PaymentCommandTest.java
+++ src/test/java/com/ledger/marketplace/loan/command/PaymentCommandTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class PaymentCommandTest {
 
     @Test
-    public void execute() {
+    public void executePaymentForShelly() {
         MarketPlace marketPlace = new MarketPlace();
         marketPlace.createLoan("UON", "Shelly", 15000, 2, 9);
 
@@ -16,5 +16,16 @@ class PaymentCommandTest {
         loanCommand.execute();
 
         assertEquals("UON Shelly 15856 3", marketPlace.getBalance("UON", "Shelly", 12));
+    }
+
+    @Test
+    public void executePaymentForHarry() {
+        MarketPlace marketPlace = new MarketPlace();
+        marketPlace.createLoan("MBI", "Harry", 10000, 3, 7);
+
+        Command loanCommand = new PaymentCommand(marketPlace, "MBI", "Harry", "5000 10");
+        loanCommand.execute();
+
+        assertEquals("MBI Harry 9044 10", marketPlace.getBalance("MBI", "Harry", 12));
     }
 }
