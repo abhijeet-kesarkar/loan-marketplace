49623e2a88c01e601fb879fcf71095d49ae0728a
Abhijeet Kesarkar <abhijit.kesar@gmail.com>
2022-01-04 23:51:53 +1100

    Refactor balance logic

--- src/main/java/com/ledger/marketplace/loan/Loan.java
+++ src/main/java/com/ledger/marketplace/loan/Loan.java
@@ -5,34 +5,36 @@ import java.util.HashMap;
 public class Loan {
     private final String bankName;
     private final String borrowerName;
-    private final int principal;
-    private final int noOfYears;
-    private final double rateOfInterest;
     private final HashMap<Integer, Integer> payments = new HashMap<>();
+    private final double amount;
+    private final int emi;
 
     public Loan(String bankName, String borrowerName, int principal, int noOfYears, double rateOfInterest) {
 
         this.bankName = bankName;
         this.borrowerName = borrowerName;
-        this.principal = principal;
-        this.noOfYears = noOfYears;
-        this.rateOfInterest = rateOfInterest;
+        double interest = principal * noOfYears * rateOfInterest / 100;
+        amount = principal + interest;
+        emi = (int) Math.ceil(amount / (noOfYears * 12));
     }
 
     public Balance balance(int emiNo) {
-        double interest = principal * noOfYears * rateOfInterest / 100;
-        double amount = principal + interest;
-        int emi = (int) Math.ceil(amount / (noOfYears * 12));
 
-        int amountPaid = payments.getOrDefault(0, 0);
-        double balanceAmount = amount - amountPaid;
+        int amountPaid = paymentDoneAtEMI(0);
         for (int i = 1; i <= emiNo; i++) {
-            amountPaid += balanceAmount < emi ? balanceAmount : emi;
-            amountPaid += payments.getOrDefault(i, 0);
-            balanceAmount = amount - amountPaid;
+            amountPaid += emiAmount(amount - amountPaid);
+            amountPaid += paymentDoneAtEMI(i);
         }
-        int noOfEMIsRemaining = (int) Math.ceil(balanceAmount / emi);
+        int noOfEMIsRemaining = (int) Math.ceil((amount - amountPaid) / emi);
         return new Balance(bankName, borrowerName, amountPaid, noOfEMIsRemaining);
+    }
+
+    private double emiAmount(double balanceAmount) {
+        return balanceAmount < emi ? balanceAmount : emi;
+    }
+
+    private Integer paymentDoneAtEMI(int i) {
+        return payments.getOrDefault(i, 0);
     }
 
     public void payment(int lumpSumAmount, int emiNo) {
